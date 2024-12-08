e05cdbd9ff86646ab8a29b3b6f242482b3d462c0
Nandini Kumari <145362258+NandiniVerma994@users.noreply.github.com>
2024-12-08 09:15:18 +0530

    Time: 4 ms (34.79%), Space: 20.4 MB (19.55%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    void dfs(int node, vector<int> adjLs[], vector<int> &vis) {
-            vis[node] = 1;
-            for(auto it: adjLs[node]) {
-                if(!vis[it]) {
-                    dfs(it, adjLs, vis);
-                }
+    void dfs(vector<int> &vis, vector<vector<int>>& adjLs, int node) {
+        vis[node] = 1;
+        for(auto it: adjLs[node]) {
+            if(!vis[it]) {
+                dfs(vis, adjLs, it);
             }
         }
+    }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
-        
-        //Step 1
-        //changing adjacency matrix to list
         int n = isConnected.size();
-        vector<int> adjLs[n];// in this adjLs is a list with empty value , adjLs is an array of 5 vectors
+        vector<vector<int>> adjLs(n);
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                //agr neigbour hoga toh adjacency matrix me 1 marked rhega
-                // but dont need to check for 0 ka neighbour 0 h ya nhi
                 if(isConnected[i][j] == 1 && i != j) {
                     adjLs[i].push_back(j);
-                    adjLs[j].push_back(i);
                 }
             }
         }
-
-        vector<int> vis(n);// vis is a vector of size n each initialised to 0
-        int cnt = 0;
+        //adjLs[[2],[1],[]]
+        int count = 0;
+        vector<int> vis(n,0);
         for(int i=0; i<n; i++) {
             if(!vis[i]) {
-                cnt++;//jitne baar dfs call hogi utne bar counter increase krte
-                dfs(i, adjLs, vis);
+                count++;
+                dfs(vis, adjLs, i);
             }
         }
-        return cnt;
+        return count;
     }
 };
