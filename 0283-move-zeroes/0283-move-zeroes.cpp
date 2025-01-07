eede97bd48d5d79bc4995cde3d0f92390d78621e
Nandini Kumari <145362258+NandiniVerma994@users.noreply.github.com>
2025-01-07 23:19:09 +0530

    Time: 0 ms (100%), Space: 23.8 MB (36.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> temp;
-        for(int i=0; i<nums.size(); i++) {
-            if(nums[i] != 0) {
-                temp.push_back(nums[i]);
+        int n = nums.size();
+        int j = -1;
+        for(int i=0; i<n; i++) {
+            if(nums[i] == 0) {
+                j = i;
+                break;
             }
         }
-        int k = temp.size();
-        int n = nums.size();
-        for(int i=0; i<k; i++) {
-            nums[i] = temp[i];
+        if(j == -1) {
+            return;
         }
-        for(int i=k; i<n; i++) {
-            nums[i] = 0;
+        for(int i=j+1; i<n; i++) {
+            if(nums[i] != 0) {
+                int temp = nums[i];
+                nums[i]  = nums[j];
+                nums[j++] = temp;
+            }
         }
     }
 };
