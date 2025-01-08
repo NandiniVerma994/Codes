fb4cb6626beae9f60d33a43d2a5920e2dbc8a619
Nandini Kumari <145362258+NandiniVerma994@users.noreply.github.com>
2025-01-08 07:38:26 +0530

    Time: 0 ms (100%), Space: 16.6 MB (79.52%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        //as we have to count the number of minutes for each traversal so use bfs
         int n = grid.size();
         int m = grid[0].size();
-        //passing row, col and time
-        queue<pair<pair<int, int>,int>> q;
-        //creating a visited matrix
         int vis[n][m];
+        queue<pair<pair<int,int>,int>> q;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                //if its 2 then mark is at visited as well as push it into the q , if its not 2 then mark vis as 0
                 if(grid[i][j] == 2) {
+                    //row, col, time
                     q.push({{i,j},0});
                     vis[i][j] = 2;
                 }
@@ -20,32 +19,32 @@ public:
             }
         }
 
-        //traversing through its neighbour
         int tm = 0;
-        int drow[] = {-1, 0, +1, 0};
-        int dcol[] = {0, 1, 0, -1};
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,+1,0,-1};
         while(!q.empty()) {
+            //t records the time of current orange
+            //tm records the time of taken by overall oranges
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
-            tm = max(tm, t);
+            tm = max(tm,t);
             q.pop();
             for(int i=0; i<4; i++) {
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {//agr oranges fresh h means grid 1 marked h
-                q.push({{nrow, ncol}, t + 1});
-                vis[nrow][ncol] = 2;
+                int nrow = delrow[i] + r;
+                int ncol = delcol[i] + c;
+                if(nrow >= 0 && nrow <n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0) {
+                    q.push({{nrow, ncol},t+1});
+                    vis[nrow][ncol] = 2;
                 }
             }
         }
 
-        //check if there exists any such cell in the grid which is still fresh then return -1
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(vis[i][j] != 2 && grid[i][j] == 1) {
                     return -1;
-                } 
+                }
             }
         }
         return tm;
