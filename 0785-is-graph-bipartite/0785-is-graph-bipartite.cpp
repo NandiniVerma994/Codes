3a964993322d875250163a086c1970ad5a469c6b
Nandini Kumari <145362258+NandiniVerma994@users.noreply.github.com>
2025-01-10 07:55:36 +0530

    Time: 7 ms (9.1%), Space: 20 MB (5.11%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,20 +1,17 @@
 class Solution {
-    //if your code fails for larger test cases its mainly bcz of components problem
-    bool check(int start, int V, vector<int> adj[], vector<int> &color) {
+public:
+    bool check(int start, int V, vector<int> &color, vector<int> adjLs[]) {
         queue<int> q;
         q.push(start);
         color[start] = 0;
         while(!q.empty()) {
             int node = q.front();
             q.pop();
-
-            for(auto it: adj[node]) {
+            for(auto it: adjLs[node]) {
                 if(color[it] == -1) {
-                    //not visited
-                    color[it] = !color[node];//paint it with the opposite color of that of the node
+                    color[it] = !color[node];
                     q.push(it);
                 }
-                //if visited then check if the adjacent nodes are painted with opposite colors
                 else if(color[it] == color[node]) {
                     return false;
                 }
@@ -23,24 +20,18 @@ class Solution {
         return true;
     }
 
-public:
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
-        //creating an adjacency list
-        vector<int> adj[V];
+        vector<int> adjLs[V];
         for(int i=0; i<V; i++) {
             for(int j=0; j<graph[i].size(); j++) {
-                adj[i].push_back(graph[i][j]);
+                adjLs[i].push_back(graph[i][j]);
             }
         }
-
         vector<int> color(V,-1);
         for(int i=0; i<V; i++) {
-            color[i] = -1;
-        }
-        for(int i=0; i<V; i++) {
             if(color[i] == -1) {
-                if(check(i, V, adj, color) == false) return false;
+                if(check(i, V, color, adjLs) == false) return false;
             }
         }
         return true;
