ecc69a5cd048f2acbcabc136114a20a95999dde0
Nandini Kumari <145362258+NandiniVerma994@users.noreply.github.com>
2025-01-11 08:45:56 +0530

    Time: 51 ms (42.52%), Space: 66.1 MB (36.14%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
+//Steps involved
+// reverse the direction of arrow and make the adjacency list .
+// now apply toposort
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int V = graph.size();
-        vector<vector<int>> adjrev(V);
+        vector<vector<int>> adjLs(V);
         vector<int> indegree(V,0);
-
         for(int i=0; i<V; i++) {
             for(auto it: graph[i]) {
-                //i -> it//edge is from i to it
-                //it -> i// reversed the edge
-                adjrev[it].push_back(i);
+                adjLs[it].push_back(i);
                 indegree[i]++;
             }
         }
-
         queue<int> q;
         for(int i=0; i<V; i++) {
             if(indegree[i] == 0) {
                 q.push(i);
             }
         }
-
+        
         vector<int> safeNodes;
         while(!q.empty()) {
             int node = q.front();
             q.pop();
             safeNodes.push_back(node);
-            for(auto it: adjrev[node]) {
+            for(auto it: adjLs[node]) {
                 indegree[it]--;
-                if(indegree[it] == 0) q.push(it);
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
             }
         }
-
         sort(safeNodes.begin(), safeNodes.end());
         return safeNodes;
     }
