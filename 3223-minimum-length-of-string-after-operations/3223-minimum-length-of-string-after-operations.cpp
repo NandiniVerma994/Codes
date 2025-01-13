a20190b1139a176ed412a05b061ed7fa5f66333c
Nandini Kumari <145362258+NandiniVerma994@users.noreply.github.com>
2025-01-13 23:48:06 +0530

    Time: 8 ms (88.71%), Space: 31.9 MB (93.55%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -1,20 +1,40 @@
+// class Solution {
+// public:
+//     int minimumLength(string s) {
+//         unordered_map<int, int> mpp;
+//         int n = s.size();
+//         for(int i=0; i<n; i++) {
+//             mpp[s[i]]++;
+//         }
+//         int totLength = 0;
+//         for(auto it: mpp) {
+//             if(it.second % 2 == 0) {
+//                 totLength += 2;
+//             }
+//             else if(it.second % 2 != 0) {
+//                 totLength += 1;
+//             }
+//         }
+//     return totLength;
+//     }
+// };
+
 class Solution {
 public:
     int minimumLength(string s) {
-        unordered_map<int, int> mpp;
-        int n = s.size();
-        for(int i=0; i<n; i++) {
-            mpp[s[i]]++;
+        vector<int> charFrequency(26, 0);
+        int totalLength = 0;
+        for (char currentChar : s) {
+            charFrequency[currentChar - 'a']++;
         }
-        int totLength = 0;
-        for(auto it: mpp) {
-            if(it.second % 2 == 0) {
-                totLength += 2;
-            }
-            else if(it.second % 2 != 0) {
-                totLength += 1;
+        for (int frequency : charFrequency) {
+            if (frequency == 0) continue;
+            if (frequency % 2 == 0) {
+                totalLength += 2;
+            } else {
+                totalLength += 1;
             }
         }
-    return totLength;
+        return totalLength;
     }
 };
